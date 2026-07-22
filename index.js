6a5aa928bd5385cdf2b67241020e707c1bc39883
labcounter <kmnenna@gmail.com>
2026-07-21 21:59:37 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+/*
+ * SoundTouch JS audio processing library
+ * Copyright (c) Olli Parviainen
+ * Copyright (c) Ryan Berdeen
+ * Copyright (c) Jakub Fiala
+ * Copyright (c) Steve 'Cutter' Blades
+ *
+ * Licensed under the Mozilla Public License, v. 2.0.
+ * You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+/**
+ * Main entry point for the SoundTouch audio-worklet package.
+ *
+ * @remarks
+ * Re-exports the SoundTouchNode class, its options types, and the processor name constant for use in host applications.
+ */
+export { SoundTouchNode } from './SoundTouchNode.js';
+export { PROCESSOR_NAME } from './constants.js';
+export { processOffline } from './processOffline.js';
+//# sourceMappingURL=index.js.map
